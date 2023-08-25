40064b4cd0722c3ef0a0cf4f1d9dc360da742ad2
Subhranshu Pujari <subh.pujari@gmail.com>
2023-08-25 12:57:57 +0530

    Adding Cards Service

---
+++ cards/src/main/java/com/mb/cards/controller/CardsController.java
@@ -0,0 +1,29 @@
+package com.mb.cards.controller;
+
+import com.mb.cards.model.Cards;
+import com.mb.cards.model.Customer;
+import com.mb.cards.repository.CardsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CardsController {
+
+    @Autowired
+    private CardsRepository cardsRepository;
+
+    @PostMapping("/myCards")
+    public List<Cards> getCardDetails(@RequestBody Customer customer) {
+        List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
+        if (cards != null) {
+            return cards;
+        } else {
+            return null;
+        }
+
+    }
+}
